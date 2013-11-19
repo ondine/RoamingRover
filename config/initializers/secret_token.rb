52317d35a01556205565a8705367ebd8eaffe251
Arun Sood <arsood@gmail.com>
2013-11-19 10:18:24 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoamingRover::Application.config.secret_key_base = '43c81a0ef82e419d8d630e688f9a60727e4fcd06666a4d53148601cdf606f1093f8577279be36081054e81f210a8c2518ba3677b2b076ee9d466fa6b99d48695'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoamingRover::Application.config.session_store :cookie_store, key: '_roaming_rover_session'
