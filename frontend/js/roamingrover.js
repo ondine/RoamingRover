6b7becae53eb5b6cd3a68b1ff29606ae67adb9e0
Arun Sood <arsood@gmail.com>
2013-11-20 14:26:20 -0800

    Updated dog walker dashboard frontend

--- frontend/js/roamingrover.js
+++ frontend/js/roamingrover.js
@@ -10,3 +10,20 @@ function resizeDogmap() {
 	$(".dog-map iframe").css("height", ($(window).height() - 80));
 	$(".dog-appt").css("height", ($(window).height() - 80));
 }
+
+/*Open Proposals*/
+
+$(".dashboard-modbox").click(function() {
+	var apptIdFull = $(this).attr("id");
+	var apptId = apptIdFull.split("appointment");
+	$("#proposal" + apptId[1]).slideToggle();
+});
+
+/*Submit proposal and close form*/
+
+$(".dashboard-proposal-submit").click(function() {
+	//AJAX here
+	
+	var propId = $(this).parents().parents().parents().parents().attr("id");
+	$("#" + propId).slideUp();
+});
